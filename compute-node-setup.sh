8649aea3d41ee75153c8cc5f6028b4b93ac17627
Jeffrey Bush <jeff@coderforlife.com>
2024-03-29 11:59:52 -0400

    Add GPU node support

--- compute-node-setup.sh
+++ compute-node-setup.sh
@@ -2,13 +2,39 @@
 
 # This script runs on the compute node of the cluster upon first boot before
 # any other cluster/Slurm setup. It initializes the following:
+#  - installing necessary packages
+#  - setting up default profiles
 #  - setting up additional SSH users
 #  - setting up node exporter
+#  - setting up NVIDIA CUDA drivers
+
+# This script must be run as root.
+
+# This script can be run on a test machine to create the AMI for the compute nodes
+# for faster booting. It will still be required to run on first boot of the compute
+# nodes as well to finish setting up the users, but the rest of the setup will be
+# MUCH faster (about 1.5-2 minutes faster - 30-40% of the boot time!).
+
+# To create AMI: (using the web console, this can be done from CLI but I haven't figured out the commands yet - only need it for steps 1, 6, and 7)
+#    1. Launch an appropriate instance with the base parallel-cluster modified Rocky Linux 8.8 AMI
+#    2. SSH into the instance
+#    3. Become root (sudo su)
+#    4. Run this script
+#    5. Run /usr/local/sbin/ami_cleanup.sh
+#    6. In the console:
+#       a. Select the instance
+#       b. Instance State > Stop
+#       c. Actions > Image and Templates > Create image
+#    7. Once fully created (the AMI becomes "Available" - this takes up to 15 minutes), delete the instance
+#    8. Update the ParallelCluster configuration to use the new AMI (Scheduling > SlurmQueues > Image > CustomAmi)
+
 
 
 ##### General setup #####
-dnf install -y nano htop valgrind gcc-toolset-13 numactl
-dnf install -y hwloc gnuplot msr-tools # tools used by the textbook
+if ! which nano &>/dev/null || ! which valgrind &>/dev/null; then
+    dnf install -y nano htop valgrind gcc-toolset-13 numactl
+    dnf install -y hwloc gnuplot msr-tools # tools used by the textbook
+fi
 
 mkdir -p -m 0755 /etc/profile.d
 
@@ -44,28 +70,32 @@ cp /etc/profile.d/gcc-version.sh /etc/profile.d/gcc-version.csh
 
 
 ##### Create users #####
-echo "Creating users..."
-USERS_GID="$(getent group users | cut -d : -f 3)"
-while read -r NEW_UID NEW_USER; do
-    useradd -d "/home/$NEW_USER" -M -N -g "$USERS_GID" -u "$NEW_UID" "$NEW_USER"
-done < "/opt/parallelcluster/shared/users.txt"
+if [ -f "/opt/parallelcluster/shared/users.txt" ]; then
+    echo "Creating users..."
+    USERS_GID="$(getent group users | cut -d : -f 3)"
+    while read -r NEW_UID NEW_USER; do
+        useradd -d "/home/$NEW_USER" -M -N -g "$USERS_GID" -u "$NEW_UID" "$NEW_USER"
+    done < "/opt/parallelcluster/shared/users.txt"
+fi
 
 
 ##### Set up node exporter #####
-echo "Setting up node exporter..."
-cat >/etc/yum.repos.d/prometheus.repo <<'EOF'
+if ! which node_exporter &>/dev/null; then
+    echo "Setting up node exporter..."
+    cat >/etc/yum.repos.d/prometheus.repo <<'EOF'
 [prometheus]
 name=prometheus
 baseurl=https://packagecloud.io/prometheus-rpm/release/el/$releasever/$basearch
 repo_gpgcheck=1
 enabled=1
 gpgkey=https://packagecloud.io/prometheus-rpm/release/gpgkey
-       https://raw.githubusercontent.com/lest/prometheus-rpm/master/RPM-GPG-KEY-prometheus-rpm
+    https://raw.githubusercontent.com/lest/prometheus-rpm/master/RPM-GPG-KEY-prometheus-rpm
 gpgcheck=1
 metadata_expire=300
 EOF
-dnf install -y node_exporter
-systemctl enable --now node_exporter
+    dnf install -y node_exporter
+    systemctl enable --now node_exporter
+fi
 
 
 ##### Termination Detection #####
@@ -93,5 +123,33 @@ systemctl enable --now node_exporter
 #     shutdown now
 # done
 # ) &
+
+##### NVIDIA CUDA Setup #####
+if lspci | grep -q NVIDIA && ! lsmod | grep -q nvidia ; then
+    # Install NVIDIA drivers
+    dnf update -y && dnf upgrade -y
+    dnf config-manager -y --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+    #dnf install kernel-devel-$(uname -r) kernel-headers-$(uname -r)
+    dnf install -y nvidia-driver nvidia-settings
+    dnf install -y cuda-driver cuda-12-4
+    
+    # Modify /usr/include/bits/floatn.h and /usr/include/bits/floatn-common.h to fix the compile-time errors with CUDA 12.4 and gcc 13.1
+    sed -i 's/^\( *# *if *!__GNUC_PREREQ *(7, *0) *||\) *defined *__cplusplus *$/\1 (defined __cplusplus \&\& !__GNUC_PREREQ (13, 0))/' /usr/include/bits/floatn.h
+    sed -i 's/^\( *# *if *!__GNUC_PREREQ *(7, *0) *||\) *defined *__cplusplus *$/\1 (defined __cplusplus \&\& !__GNUC_PREREQ (13, 0))/' /usr/include/bits/floatn-common.h
+
+    # Reboot to load the NVIDIA driver
+    reboot now
+fi
+
+if lspci | grep -q NVIDIA; then
+    # Set up CUDA environment variables
+    cat >/etc/profile.d/cuda.sh <<'EOF'
+export PATH="/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+EOF
+    chmod 644 /etc/profile.d/cuda.sh
+    cp /etc/profile.d/cuda.sh /etc/profile.d/cuda.csh
+fi
+
 
 exit 0
